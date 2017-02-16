5fa115c7e37eaa6a5ed633032e1152e3e4693a3c
sofiane <degoui.sofiane@gmail.com>
2017-02-16 10:57:47 +0100

    Ajout du bouton toggle land et takeoff. Ce commit fait voler les drones synchrones.

--- app/src/main/java/com/eisc/claryo/swamdrones/Control.java
+++ app/src/main/java/com/eisc/claryo/swamdrones/Control.java
@@ -3,9 +3,12 @@ package com.eisc.claryo.swamdrones;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
 /**
  * Classe pour l'interface de controle de vol de l'essaim
@@ -24,7 +27,7 @@ public class Control extends AppCompatActivity {
 
         ImageButton btnRetour = (ImageButton) findViewById(R.id.btnRetourMenuPrincipal1);
         ImageButton btnSettings = (ImageButton) findViewById(R.id.btnSettings);
-        ImageButton btn_takeoff_land = (ImageButton) findViewById(R.id.btn_takeoff_land);
+        ToggleButton toggle_takeoff_land = (ToggleButton) findViewById(R.id.toggle_takeoff_land);
 
         ImageButton btn_forward = (ImageButton) findViewById(R.id.btn_forward);
         ImageButton btn_roll_left = (ImageButton) findViewById(R.id.btn_roll_left);
@@ -60,29 +63,250 @@ public class Control extends AppCompatActivity {
             }
         });
 
-        btn_takeoff_land.setOnClickListener(new View.OnClickListener() {
+        toggle_takeoff_land.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                for (int i = 0;i<MainActivity.listBebop.size();i++){
-                    MainActivity.listBebop.get(i).takeOff();
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                        MainActivity.listBebop.get(i).takeOff();
+                    }
                 }
-
+                else {
+                    for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                        MainActivity.listBebop.get(i).land();
+                    }
+                }
             }
         });
+
         btn_emergency.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                for (int i = 0; i<MainActivity.listBebop.size();i++){
+                for (int i = 0; i < MainActivity.listBebop.size(); i++) {
                     MainActivity.listBebop.get(i).emergency();
                 }
             }
         });
-        btn_forward.setOnClickListener(new View.OnClickListener() {
+        btn_forward.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                            MainActivity.listBebop.get(i).setPitch((byte) 50);
+                            MainActivity.listBebop.get(i).setFlag((byte) 1);
+                        }
+                        break;
 
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                            MainActivity.listBebop.get(i).setPitch((byte) 0);
+                            MainActivity.listBebop.get(i).setFlag((byte) 0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
             }
         });
+        btn_back.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                            MainActivity.listBebop.get(i).setPitch((byte) -50);
+                            MainActivity.listBebop.get(i).setFlag((byte) 1);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                            MainActivity.listBebop.get(i).setPitch((byte) 0);
+                            MainActivity.listBebop.get(i).setFlag((byte) 0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+
+        });
+
+        btn_roll_left.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                            MainActivity.listBebop.get(i).setRoll((byte) -50);
+                            MainActivity.listBebop.get(i).setFlag((byte) 1);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                            MainActivity.listBebop.get(i).setRoll((byte) 0);
+                            MainActivity.listBebop.get(i).setFlag((byte) 0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+
+        btn_roll_right.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                            MainActivity.listBebop.get(i).setRoll((byte) 50);
+                            MainActivity.listBebop.get(i).setFlag((byte) 1);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                            MainActivity.listBebop.get(i).setRoll((byte) 0);
+                            MainActivity.listBebop.get(i).setFlag((byte) 0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+        btn_yaw_left.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                            MainActivity.listBebop.get(i).setYaw((byte) -50);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                            MainActivity.listBebop.get(i).setYaw((byte) 0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+        btn_yaw_right.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                            MainActivity.listBebop.get(i).setYaw((byte) 50);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                            MainActivity.listBebop.get(i).setYaw((byte) 0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+        btn_gaz_up.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                            MainActivity.listBebop.get(i).setGaz((byte) 50);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                            MainActivity.listBebop.get(i).setGaz((byte) 0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+        btn_gaz_down.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setPressed(true);
+                        for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                            MainActivity.listBebop.get(i).setGaz((byte) -50);
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        v.setPressed(false);
+                        for (int i = 0; i < MainActivity.listBebop.size(); i++) {
+                            MainActivity.listBebop.get(i).setGaz((byte) 0);
+                        }
+                        break;
+
+                    default:
+
+                        break;
+                }
+
+                return true;
+            }
+        });
+
        /* btnSwapView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/eisc/claryo/swamdrones/DiscoveryDrone.java
+++ app/src/main/java/com/eisc/claryo/swamdrones/DiscoveryDrone.java
@@ -122,7 +122,7 @@ public class DiscoveryDrone implements ARDiscoveryServicesDevicesListUpdatedRece
                 }
                 else {
                     //Construction
-                    MainActivity.listBebop.get(0).dispose();
+                    //MainActivity.listBebop.get(0).dispose();
                     for (int i = 0;i<deviceList.size();i++){
                         MainActivity.listBebop.add(new BebopDrone(context,deviceList.get(i)));
                         boolean isConnect = MainActivity.listBebop.get(i).connect();
